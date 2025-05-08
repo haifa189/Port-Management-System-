f18cc4ae0b9378b0dc2199dfc5fdc7d60810e3db
haifasidani <haifasidani00@gmail.com>
2025-05-08 19:49:40 +0300

    Update BasicContainer.java

--- src/container/BasicContainer.java
+++ src/container/BasicContainer.java
@@ -1,1 +1,29 @@
+package container;
 
+public class BasicContainer extends Container {
+	public BasicContainer(int ID, int weight, boolean isOnShip, boolean isOnPort) {
+		super(ID, weight, isOnShip, isOnPort);
+	}
+
+	public BasicContainer() {
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		if (weight <= 5000) {
+			super.setWeight(weight);
+		} else {
+			System.out.println("This is a heavy container.");
+		}
+	}
+
+	@Override
+	public double fuelConsumption() {
+		return (2.50 * super.getWeight());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nType: Basic Container }";
+	}
+}
